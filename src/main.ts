950bc32ca1fc07f1c9d0fee77fa5d5131c75bee5
Otavio G <oglamberty@inf.ufsm.br>
2024-05-27 19:43:03 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { registerPlugins } from '@/plugins'
+
+import App from './App.vue'
+
+import { createApp } from 'vue'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
